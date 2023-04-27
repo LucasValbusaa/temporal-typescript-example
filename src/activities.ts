cc1b866918204b3da1d2668f78c2570bba5c6421
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-04-26 21:10:53 -0300

    feat: add type coffee props

--- src/activities.ts
+++ src/activities.ts
@@ -1,4 +1,9 @@
-export async function requestACoffee(): Promise<any> {
+type CoffeeProps = {
+  name: string;
+  type: string;
+};
+
+export async function requestACoffee(): Promise<CoffeeProps> {
   console.log('Request a coffee from the coffee-maker-api');
   return {
     name: 'Black Coffee',
